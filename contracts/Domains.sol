26bdb763a91752b8fafc14ffcba3bddb800f0cf3
Pablo Poggio <pablopoggiog@gmail.com>
2022-02-19 14:37:15 -0300

    Implement price to register domains

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,52 +2,81 @@
 
 pragma solidity ^0.8.10;
 
+import {StringUtils} from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
-/*
+/**
  * @title Domains
  * Domains - Contract that creates and stores domains in the blockchain.
  */
 contract Domains {
-    /*
-     * @notice - stores addresses of the owners of all the domains, indexed by domain's name.
+    /**
+     * @dev - Stores the domain's TLD.
+     */
+    string public tld;
+    /**
+     * @notice - Stores addresses of the owners of all the domains, indexed by domain's name.
      */
     mapping(string => address) public domains;
-    /*
-     * @notice - stores records for domains, indexed by domain's name.
+    /**
+     * @notice - Stores records for domains, indexed by domain's name.
      */
     mapping(string => string) public records;
 
-    /*
-     * @dev - for now, it only logs a string to the console.
+    /**
+     * @dev - Sets the value for `tld` and logs it to the console.
+     * @param _tld - The value to be set for `tld`.
      */
-    constructor() {
-        console.log("Yo yo, I'm a contract and I'm smart");
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
     }
 
-    /*
-     * @notice - registers a domain name, mapping it to the caller's address.
-     * @param name - the name for the domain to be registered.
+    /**
+     * @notice - Gives the price of a domain based on its name's length.
+     * @param name - The name for the domain to be registered.
+     * @return - The price for that domain.
      */
-    function register(string calldata name) public {
+    function getPrice(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; // 0.5 MATIC
+        } else if (len == 4) {
+            return 3 * 10**17;
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    /**
+     * @notice - Registers a domain name, mapping it to the caller's address.
+     * @param name - The name for the domain to be registered.
+     */
+    function register(string calldata name) public payable {
         require(domains[name] == address(0), "Name is already in use");
+
+        uint256 _price = getPrice(name);
+
+        require(msg.value >= _price, "Not enough MATIC paid");
 
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
 
-    /*
-     * @notice - returns the address registered for a specific domain name.
-     * @param name - the domain's name.
+    /**
+     * @notice - Returns the address registered for a specific domain name.
+     * @param name - The domain's name.
+     * @return - The address registered for that domain.
      */
     function getAddress(string calldata name) public view returns (address) {
         return domains[name];
     }
 
-    /*
-     * @notice - sets a record for a specific domain name.
-     * @param name - the domain's name.
-     * @param record - the record to store.
+    /**
+     * @notice - Sets a record for a specific domain name.
+     * @param name - The domain's name.
+     * @param record - The record to store.
      */
     function setRecord(string calldata name, string calldata record) public {
         require(
@@ -58,9 +87,10 @@ contract Domains {
         records[name] = record;
     }
 
-    /*
+    /**
      * @notice - returns a record stored for a specific domain name.
-     * @param name - the domain's name.
+     * @param name - The domain's name.
+     * @return - The record stored in that domain.
      */
     function getRecord(string calldata name)
         public
--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,21 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const TLD = "zed";
+  const domainContract = await domainContractFactory.deploy(TLD);
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
 
-  let txn = await domainContract.register("doom");
+  let txn = await domainContract.register("mortal", {
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
   await txn.wait();
 
-  const domainAddress = await domainContract.getAddress("doom");
-  console.log("Owner of domain doom:", domainAddress);
+  const address = await domainContract.getAddress("mortal");
+  console.log(`Owner of domain mortal.${TLD}:`, address);
 
-  txn = await domainContract
-    .connect(randomPerson)
-    .setRecord("doom", "Haha my domain now!");
-  await txn.wait();
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
