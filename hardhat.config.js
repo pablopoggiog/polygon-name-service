c18d751cbdf58e8ceb5b5236678b53862dcbfd50
Pablo Poggio <pablopoggiog@gmail.com>
2022-02-19 13:29:35 -0300

    Set up docgen

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-docgen");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.10",
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  },
 };
