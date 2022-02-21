ad9864bbdb31b47ddd39c9e765fa390f09aa6266
Pablo Poggio <pablopoggiog@gmail.com>
2022-02-20 21:12:26 -0300

    Improve Button's styling

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,18 +1,33 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+`;
 
 export const Button = styled.button`
   border-radius: 1em;
   padding: 14px;
   cursor: pointer;
-  background: rgba(130, 71, 229, 0.7);
+  background: -webkit-linear-gradient(left, #a200d6, rgba(130, 71, 229, 0.7));
+  background-size: 200% 200%;
   width: 100%;
   border: none;
+  color: linear-gradient(left, #a200d6, rgba(130, 71, 229, 0.7));
   color: white;
   font-size: 1em;
+  animation: ${gradientAnimation} 4s ease infinite;
+  transition: 0.5s;
 
   &:hover {
-    background: white;
-    color: black;
-    transition: 0.5s;
+    transform: scale(1.02);
   }
 `;
