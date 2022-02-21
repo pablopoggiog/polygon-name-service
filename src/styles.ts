6564b1e8d7110e2d029b5d1f0b9c4ca256a353a7
Pablo Poggio <pablopoggiog@gmail.com>
2022-02-20 21:14:47 -0300

    Changed app's from 100vh height to min-height

--- src/styles.ts
+++ src/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #081018;
   color: white;
   align-items: center;
